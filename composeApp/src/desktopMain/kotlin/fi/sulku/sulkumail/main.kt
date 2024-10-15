626aa5a5a44547c83165634d8563208f7399cb3c
Zoltus <zoltus@outlook.com>
2024-10-15 12:28:47 +0300

    feat(desktop): center window and window size to content

--- composeApp/src/desktopMain/kotlin/fi/sulku/sulkumail/main.kt
+++ composeApp/src/desktopMain/kotlin/fi/sulku/sulkumail/main.kt
@@ -1,14 +1,23 @@
 package fi.sulku.sulkumail
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import fi.sulku.sulkumail.koin.initKoin
 
 fun main() = application {
     initKoin()
     Window(
-        onCloseRequest = ::exitApplication,
         title = "SulkuMail",
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(
+            width = 1200.dp,
+            height = 800.dp,
+            position = WindowPosition(Alignment.Center)
+        ),
     ) {
         App()
     }
