b4008ee1d7dcaa94abd14da6a308750d9db65363
Zoltus <zoltus@outlook.com>
2024-10-24 18:35:30 +0300

    feat(front): prepare switching to new typesafe navigation system

--- composeApp/src/commonMain/kotlin/fi/sulku/sulkumail/App.kt
+++ composeApp/src/commonMain/kotlin/fi/sulku/sulkumail/App.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -15,11 +14,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import fi.sulku.sulkumail.composables.login.Login
+import androidx.navigation.toRoute
 import fi.sulku.sulkumail.composables.sidebar.SideDrawer
 import fi.sulku.sulkumail.composables.sidebar.mails
 import fi.sulku.sulkumail.theme.AppTheme
 import fi.sulku.sulkumail.theme.CustomColor
+import kotlinx.serialization.Serializable
 import org.koin.compose.KoinContext
 
 @Composable
@@ -36,38 +36,53 @@ fun App() = AppTheme {
                 .fillMaxSize()
                 .background(CustomColor.discordDark)
         ) {
-            // todo topbar remove from expect,
-            // todo add only from desktopmain & wasm access drawerstate from vm
-            //TopBar(loggedIn, drawerState)
-
             SideDrawer(nav, drawerState) {
                 NavHost(
                     navController = nav,
-                    startDestination = "login",
+                    startDestination = WelcomeRoute,
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(1.dp)
                 ) {
-                    // Route for welcome screen
-                    composable(route = "welcome") {
-                        WelcomeScreen()
+                    composable<WelcomeRoute> {
+                        nav.navigate(WelcomeRoute)
                     }
                     // Login
-                    composable(route = "login") {
-                        Login()
+                    composable<LoginRoute> {
+                        nav.navigate(LoginRoute)
                     }
                     // Routes for emails
                     mails.forEach { mail ->
+                        composable<MailRoute> { backStackEntry ->
+                            val mailRoute: MailRoute = backStackEntry.toRoute()
+                            MailRoute(mailRoute.email)
+                            SearchBarSample(drawerState)
+                        }
+                        /*
+                        composable<MailRoute> {
+                            nav.navigate(MailRoute(mail.email))
+                            SearchBarSample(drawerState)
+                        }
                         composable(route = mail.email) {
                             //MailScreen(mail.email)
                             SearchBarSample(drawerState)
                         }
+                         */
                     }
                 }
             }
         }
     }
 }
+
+@Serializable
+data class MailRoute(val email: String)
+
+@Serializable
+object WelcomeRoute
+
+@Serializable
+object LoginRoute
 
 @Composable
 fun MailScreen(email: String) {
@@ -78,3 +93,6 @@ fun MailScreen(email: String) {
 fun WelcomeScreen() {
     Text("Welcome to Sulkumail")
 }
+
+
+
