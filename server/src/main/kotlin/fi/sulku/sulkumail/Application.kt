e5a9e71bc2dda24832253ea0c7b5e04bd33fc15e
Zoltus <zoltus@outlook.com>
2025-02-02 00:12:33 +0200

    feat(server): add routes for auth&messages

--- server/src/main/kotlin/fi/sulku/sulkumail/Application.kt
+++ server/src/main/kotlin/fi/sulku/sulkumail/Application.kt
@@ -5,6 +5,8 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.serialization.json.Json
@@ -14,20 +16,33 @@ fun main() {
         Netty, port = 8080,
         host = "0.0.0.0",
         module = Application::module,
-        ).start(wait = true)
+    ).start(wait = true)
 }
 
 @OptIn(DelicateCoroutinesApi::class)
 fun Application.module() {
-
     install(ContentNegotiation) {
-        json(Json { prettyPrint = true })
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
     }
 
     routing {
-        route("/auth") {
-            post("/register") {
+        post("/auth") {
+            val req = call.receive<TokenRequest>()
+            when (req.provider) {
+                Provider.GOOGLE -> {
+                    call.respond(gTokenRequest(req))
+                }
+                Provider.OUTLOOK -> {}
             }
+        }
+        post("/messages") {
+            val req = call.receive<MessageListRequest>()
+            val messagesResp: MessageListResponse = gFetchMessageList(req)
+            val messageDetails: EmailDetailResp = gFetchEmailDetails(req.access_token, messagesResp)
+            call.respond(messageDetails)
         }
     }
 }
