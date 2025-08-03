fcc6d1c9147e723d2592f1545a43f90806b03c34
Zoltus <zoltus@outlook.com>
2025-08-03 16:10:08 +0300

    feat(front): implement ai chat copy button & remove unused svg

--- composeApp/src/commonMain/kotlin/fi/sulku/sulkumail/composables/screens/mail_editor/tools/ai/ResponseField.kt
+++ composeApp/src/commonMain/kotlin/fi/sulku/sulkumail/composables/screens/mail_editor/tools/ai/ResponseField.kt
@@ -1,19 +1,27 @@
 package fi.sulku.sulkumail.composables.screens.mail_editor.tools.ai
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ResponseField(
     message: String,
-    onCopy: () -> Unit = {}
 ) {
+    // Deprecated, new api hasn't fully implemented cross platform yet.
+    val clipboard = LocalClipboardManager.current
     Box {
         TextField(
             value = message,
@@ -23,7 +31,7 @@ fun ResponseField(
         )
 
         IconButton(
-            onClick = onCopy,
+            onClick = { clipboard.setText(AnnotatedString(message)) },
             modifier = Modifier
                 .align(Alignment.TopEnd)
                 .padding(4.dp)
